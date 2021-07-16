861f4f1e1467993f3f3e18e980e801fcd5679b26
TheDestroyer19 <17345449+TheDestroyer19@users.noreply.github.com>
2021-07-16 13:53:26 -0700

    Introduced Listeditor

---
+++ js/listEditor.js
@@ -0,0 +1,131 @@
+import { RemoveAllChildNodes } from './common.js';
+import {Modal} from './modal.js';
+
+/**
+ * @typedef {function} ModalOpenCallback
+ * @param {Object} entry - the entry being opened in the modal
+ */
+
+/**
+ * @typedef {function} UpdateDisplayCallback
+ * @param {Object} entry - the entry that was edited
+ * @param {HTMLElement} element - the element that displays the entry being edited
+ */
+
+/**
+ * @typedef {function} UpdateEntryCallback
+ * @param {Object} entry - the entry that is being edited
+ * Note that the UpdateDisplayCallback will be called afterwards
+ */
+
+/**
+ * @typedef {function} deleteEntryCallback
+ * @param {Object} entry - the entry that is being deleted
+ * Note: the element has already been removed from display and the managed array
+ */
+
+export class ListEditor {
+
+    constructor(
+        getDataArray,
+        htmlTemplateID,
+        htmlContainerID,
+        htmlModalID,
+        modalOpenCallback,
+        updateDisplayCallback,
+        updateEntryCallback,
+        deleteEntryCallback,
+    ) {
+        this.getDataArray = getDataArray;
+        /** @private @type {!HTMLTemplateElement}*/
+        this.template = document.getElementById(htmlTemplateID);
+        /** @private @type {!HTMLElement}*/
+        this.container = document.getElementById(htmlContainerID);
+        /** @private @type {!Modal} */
+        this.modal = new Modal(
+            document.getElementById(htmlModalID),
+            () => this.onModalSave(),
+            () => this.onModalDelete(),
+        );
+        /** @private @type {?Object} */
+        this.activeEntry = null;
+        /** @private @type {?HTMLElement} */
+        this.activeElement = null;
+
+        /** @type {ModalOpenCallback} */
+        this.modalOpenCallback = modalOpenCallback;
+        /** @type {UpdateDisplayCallback} */
+        this.updateDisplayCallback = updateDisplayCallback;
+        /** @type {UpdateEntryCallback} */
+        this.updateEntryCallback = updateEntryCallback;
+        this.deleteEntryCallback = deleteEntryCallback;
+    }
+
+    regenerate() {
+        RemoveAllChildNodes(this.container);
+        this.activeElement = null;
+        this.activeEntry = null;
+        this.getDataArray().forEach(entry => this.addDisplay(entry));
+    }
+
+    /**
+     * @private
+     * @param {Object} entry the entry to generate display for
+     */
+    addDisplay(entry, editNow = false) {
+        /** @type {HTMLElement} */
+        let element = this.template.content.firstElementChild.cloneNode(true);
+        this.updateDisplayCallback(entry, element);
+
+        let edit = () => {
+            this.activeEntry = entry;
+            this.activeElement = element;
+
+            this.modalOpenCallback(entry);
+
+            this.modal.Open();
+        };
+
+        element.querySelector('.edit').addEventListener('click', edit);
+
+        this.container.appendChild(element);
+
+        if (editNow) edit();
+    }
+
+    /**
+     * Adds a new entry to the list this editor controls, and opens it for editing
+     * @param {Object} entry the entry being added
+     */
+    add(entry) {
+        this.getDataArray().push(entry);
+        this.addDisplay(entry, true);
+    }
+
+    /**
+     * @private
+     */
+    onModalSave() {
+        if (!this.activeEntry) return;
+        this.updateEntryCallback(this.activeEntry);
+        this.updateDisplayCallback(this.activeEntry, this.activeElement);
+        this.activeEntry = null;
+        this.activeElement = null;
+    }
+
+    /**
+     * @private
+     */
+    onModalDelete() {
+        if (!this.activeEntry) return;
+
+        let idx = this.getDataArray().findIndex(e => e === this.activeEntry);
+        if (idx >= 0) this.getDataArray().splice(idx, 1);
+
+        this.activeElement.remove();
+        this.deleteEntryCallback(this.activeEntry);
+        this.activeEntry = null;
+        this.activeElement = null;
+
+    }
+}
--- js/weapon.js
+++ js/weapon.js
@@ -1,9 +1,6 @@
-import { CHARACTER_LOADED, RemoveAllChildNodes, SendCharacterUpdated } from './common.js';
-import {Modal} from './modal.js';
+import { CHARACTER_LOADED, SendCharacterUpdated } from './common.js';
 import {COMBAT_SKILL_NAME, RANGE, Weapon} from './genesys.js';
-
-const WeaponTemplate = document.getElementById("weapon-template");
-const WeaponTable = document.getElementById("weapons-table");
+import { ListEditor } from './listEditor.js';
 
 const WeaponNameInput = document.getElementById("edit-weapon-name");
 const WeaponSkillInput = document.getElementById("edit-weapon-skill");
@@ -14,102 +11,56 @@ const WeaponSpecialInput = document.getElementById("edit-weapon-special");
 
 const NewWeapon = document.getElementById('new-weapon');
 
-const WeaponModal = new Modal(
-    document.getElementById("modal-edit-weapon"),
-    UpdateActiveWeaponData,
-    DeleteActiveWeapon,
+const WeaponListEditor = new ListEditor(
+    () => window.character.weapons,
+    "weapon-template",
+    'weapons-table',
+    'modal-edit-weapon',
+    UpdateModal, 
+    UpdateDisplay,
+    UpdateWeapon,
+    () => SendCharacterUpdated(),
 );
 
-let activeWeapon = null;
-let activeElement = null;
-
-function UpdateActiveWeaponData() {
-    activeWeapon.name = WeaponNameInput.value;
-    activeWeapon.skill = WeaponSkillInput.value;
-    activeWeapon.damage = parseInt(WeaponDamageInput.value);
-    activeWeapon.crit = parseInt(WeaponCriticalInput.value);
-    activeWeapon.range = WeaponRangeInput.value;
-    activeWeapon.special = WeaponSpecialInput.value;
-
-    UpdateWeaponDisplay(activeElement, activeWeapon);
-
-    SendCharacterUpdated();
+/**
+ * @param {Weapon} entry 
+ * @param {HTMLElement} element 
+ */
+function UpdateDisplay(entry, element) {
+    element.querySelector(".name").textContent = entry.name;
+    element.querySelector(".skill").textContent = entry.skill;
+    element.querySelector(".damage").textContent = entry.damage;
+    element.querySelector(".crit").textContent = entry.crit;
+    element.querySelector(".range").textContent = entry.range;
+    element.querySelector(".special").textContent = entry.special;
 }
 
 /**
- * 
  * @param {Weapon} weapon 
  */
-function UpdateWeaponDisplay(element, weapon) {
-    element.querySelector(".name").textContent = weapon.name;
-    element.querySelector(".skill").textContent = weapon.skill;
-    element.querySelector(".damage").textContent = weapon.damage;
-    element.querySelector(".crit").textContent = weapon.crit;
-    element.querySelector(".range").textContent = weapon.range;
-    element.querySelector(".special").textContent = weapon.special;
+function UpdateModal(weapon) {
+    WeaponNameInput.value = weapon.name;
+    WeaponSkillInput.value = weapon.skill;
+    WeaponDamageInput.value = weapon.damage;
+    WeaponCriticalInput.value = weapon.crit;
+    WeaponRangeInput.value = weapon.range;
+    WeaponSpecialInput.value = weapon.special;
 }
 
-/**
- * 
- * @param {Weapon} weapon 
- */
-function AddWeapon(weapon, editNow = false) {
-    /** @type {HTMLTableRowElement} */
-    let weaponRow = WeaponTemplate.content.cloneNode(true);
-    weaponRow = weaponRow.querySelector(".weapon");
-
-    UpdateWeaponDisplay(weaponRow, weapon);
-
-    let edit = () => {
-        activeElement = weaponRow;
-        activeWeapon = weapon;
-
-        WeaponNameInput.value = weapon.name;
-        WeaponSkillInput.value = weapon.skill;
-        WeaponDamageInput.value = weapon.damage;
-        WeaponCriticalInput.value = weapon.crit;
-        WeaponRangeInput.value = weapon.range;
-        WeaponSpecialInput.value = weapon.special;
-
-        WeaponModal.Open();
-    }
-
-    weaponRow.querySelector(".edit").addEventListener("click", edit);
-
-    if (editNow) edit();
-
-    WeaponTable.appendChild(weaponRow);
-}
-
-function DeleteActiveWeapon() {
-    let character = window.character;
-
-    let idx = character.weapons.findIndex(e => e === activeWeapon);
-    if (idx >= 0) character.weapons.splice(idx, 1);
-
-    activeElement.remove();
-    activeWeapon = null;
-    activeElement = null;
+function UpdateWeapon(weapon) {
+    weapon.name = WeaponNameInput.value;
+    weapon.skill = WeaponSkillInput.value;
+    weapon.damage = parseInt(WeaponDamageInput.value);
+    weapon.crit = parseInt(WeaponCriticalInput.value);
+    weapon.range = WeaponRangeInput.value;
+    weapon.special = WeaponSpecialInput.value;
 
     SendCharacterUpdated();
 }
-
-function AddAllWeapons() {
-    let weapons = window.character.weapons;
-
-    RemoveAllChildNodes(WeaponTable);
-
-    weapons.forEach(weapon => {
-        AddWeapon(weapon, false);
-    });
-}
-
-document.addEventListener(CHARACTER_LOADED, AddAllWeapons);
 
 NewWeapon.addEventListener('click', () => {
-    let character = window.character;
-    let weapon = new Weapon("Unnamed weapon", COMBAT_SKILL_NAME.Melee, 0, 0, RANGE.Engaged, "");"Melee"
-    character.weapons.push(weapon);
-    AddWeapon(weapon, true);
+    WeaponListEditor.add(new Weapon("Unnamed weapon", COMBAT_SKILL_NAME.Melee, 0, 0, RANGE.Engaged, ""));
+    SendCharacterUpdated();
 });
 
+document.addEventListener(CHARACTER_LOADED, () => WeaponListEditor.regenerate());
