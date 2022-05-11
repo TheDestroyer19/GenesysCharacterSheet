b982fa6b821bf9d1bb7e52ca842c237dc8f6beb5
TheDestroyer19 <17345449+TheDestroyer19@users.noreply.github.com>
2022-05-10 17:34:28 -0700

    merged ability and ability-display

--- js/ability.js
+++ js/ability.js
@@ -1,7 +1,93 @@
 import { CHARACTER_LOADED } from "./common.js";
 import { Ability } from './genesys.js';
 import { NewSimpleListEditor } from "./listEditor.js";
-import { AbilityDisplay } from './elements/ability-display.js';
+import { ConvertSymbols } from "./util/prettyText.js";
+import { } from "./elements/list-controls.js";
+
+export class AbilityDisplay extends HTMLElement {
+    #state;
+
+    constructor() {
+        super();
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.innerHTML = /* HTML */ `
+        <style>
+        @import '/css/shared.css';
+        
+        :host {
+            display: flex;
+            flex-direction: row;
+            gap: 0.25rem;
+            margin-top: 0.25rem;
+            margin-bottom: 0.25rem;
+            font-size: small;
+        }
+        h1, h2 {
+            display: inline;
+            font-size: small;
+        }
+        h1 {
+            font-weight: bold;
+        }
+        h2 {
+            font-weight: normal;
+        }
+        </style>
+        <list-controls></list-controls>
+        <div>
+            <div>
+                <button id="edit" class="edit" title="Edit">🖉</button>
+                <h1 id="name">Name</h1>
+                <h2 id="source">Source</h2>
+            </div>
+            <div id="body">
+                "body"
+            </div>
+        </div>
+        `;
+
+        this.shadowRoot.getElementById('edit').addEventListener('click', event => {
+            event.preventDefault();
+            event.target.blur();
+            this.#edit(event);
+        });
+        
+        this.#state = {};
+        this.onEdit = event => console.warn("Ability-Display needs onEdit set");
+
+    }
+
+    static get observedAttributes() {
+        return ['name', 'source', 'description'];
+    }
+
+    static get tag() {
+        return "ability-display";
+    }
+
+    connectedCallback() {
+        if (!this.isConnected) return;
+
+        ConvertSymbols(this.#state.name, this.shadowRoot.querySelector('#name'));
+        ConvertSymbols(this.#state.source, this.shadowRoot.querySelector('#source'));
+        ConvertSymbols(this.#state.description, this.shadowRoot.querySelector('#body'));
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.#state[name] = newValue;
+        switch (name) {
+            case 'name': ConvertSymbols(newValue, this.shadowRoot.querySelector('#name')); break;
+            case 'source': ConvertSymbols(newValue, this.shadowRoot.querySelector('#source')); break;
+            case 'description': ConvertSymbols(newValue, this.shadowRoot.querySelector('#body')); break;
+        }
+    }
+
+    #edit(event) {
+        this.onEdit(event);
+    }
+}
+customElements.define(AbilityDisplay.tag, AbilityDisplay);
 
 const ModalTemplate = document.createElement('template');
 ModalTemplate.id = 'ability-modal-template';
