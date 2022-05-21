43660880d8c77149b1cad4fa8a91ad00006e318e
TheDestroyer19 <17345449+TheDestroyer19@users.noreply.github.com>
2022-05-21 12:33:23 -0700

    implemented load character

--- genesys-characters/src-tauri/src/main.rs
+++ genesys-characters/src-tauri/src/main.rs
@@ -9,10 +9,10 @@ use std::fs::File;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Mutex;
 
+use anyhow::Context;
 use genesys::Character;
 use tauri::api::dialog;
 use tauri::{CustomMenuItem, Manager, Menu, Submenu, Window, WindowMenuEvent};
-use anyhow::Context;
 
 struct CharacterDirty(AtomicBool);
 
@@ -28,45 +28,44 @@ fn on_character_edited(
     println!("Character modified");
 }
 
-fn save_character(
-    window: Window,
-) {
-    let name= {
-      let character = window.state::<Mutex<Character>>();
-      let character = character.lock().unwrap();
-      format!("{}.json", character.header.name)
+fn save_character(window: Window) {
+    let name = {
+        let character = window.state::<Mutex<Character>>();
+        let character = character.lock().unwrap();
+        format!("{}.json", character.header.name)
     };
 
     dialog::FileDialogBuilder::new()
         .set_file_name(&name)
         .save_file(move |file_path| {
-          if file_path.is_none() {
-              return;
-          }
-          let file_path = file_path.unwrap();
+            if file_path.is_none() {
+                return;
+            }
+            let file_path = file_path.unwrap();
 
-          let character = window.state::<Mutex<Character>>();
-          let character = character.lock().unwrap();
-      
-          let result = File::create(&file_path)
-            .with_context(|| format!("Failed to create file '{}'", file_path.display()))
-            .and_then(|file| serde_json::to_writer(file, &*character).context("Failed to serialize data"));
-      
-          match result {
-            Ok(()) => window.state::<CharacterDirty>().0.store(false, Ordering::SeqCst),
-            Err(e) => dialog::message(
-              Some(&window),
-              "Failed to save character",
-              format!("{}", e)
-            ),
-          }
+            let character = window.state::<Mutex<Character>>();
+            let character = character.lock().unwrap();
+
+            let result = File::create(&file_path)
+                .with_context(|| format!("Failed to create file '{}'", file_path.display()))
+                .and_then(|file| {
+                    serde_json::to_writer(file, &*character).context("Failed to serialize data")
+                });
+
+            match result {
+                Ok(()) => window
+                    .state::<CharacterDirty>()
+                    .0
+                    .store(false, Ordering::SeqCst),
+                Err(e) => {
+                    dialog::message(Some(&window), "Failed to save character", format!("{}", e))
+                }
+            }
         });
 }
 
 fn new_character(window: Window) {
-    let dirty = {
-        window.state::<CharacterDirty>().0.load(Ordering::SeqCst)
-    };
+    let dirty = { window.state::<CharacterDirty>().0.load(Ordering::SeqCst) };
     let new_character = move || {
         let character_state = window.state::<Mutex<Character>>();
         let dirty = window.state::<CharacterDirty>();
@@ -91,11 +90,57 @@ fn new_character(window: Window) {
     }
 }
 
+fn open_character(window: Window) {
+    let dirty = { window.state::<CharacterDirty>().0.load(Ordering::SeqCst) };
+    let load_character = |window: Window| {
+        dialog::FileDialogBuilder::new()
+            .pick_file(move |file| {
+                if file.is_none() {
+                    return;
+                }
+                let path = file.unwrap();
+
+                let loaded_character = File::open(&path).with_context(|| format!("Failed to open '{}'", path.display()))
+                    .and_then(|file| serde_json::from_reader(file).context("Failed to load character"));
+
+                match loaded_character {
+                    Ok(loaded_character) => {
+                        let character_state = window.state::<Mutex<Character>>();
+                        let mut character = character_state.lock().unwrap();
+                        let dirty = window.state::<CharacterDirty>();
+                        *character = loaded_character;
+                        dirty.0.store(false, Ordering::SeqCst);
+                        window
+                            .app_handle()
+                            .emit_all("character-updated", character.clone())
+                            .unwrap();
+                    },
+                    Err(e) => dialog::message(Some(&window), "Failed to save character", format!("{}", e))
+                }
+            });
+    };
+
+    if dirty {
+        dialog::ask(
+            Some(&window.clone()),
+            "New Character",
+            "The character has unsaved changes.\nDo you want to discard those changes?",
+            move |answer| {
+                if answer {
+                    load_character(window);
+                }
+            },
+        );
+    } else {
+        load_character(window);
+    }
+}
+
 fn on_menu_event(event: WindowMenuEvent) {
     let window = event.window();
     match event.menu_item_id() {
         "new" => new_character(window.clone()),
-        "open" => todo!(),
+        "open" => open_character(window.clone()),
         "save" => save_character(window.clone()),
         a => println!("Unhandled menu event '{}'", a),
     }
@@ -107,6 +152,9 @@ fn get_character(character: tauri::State<Mutex<Character>>) -> Character {
 }
 
 fn main() {
+    let thread = std::thread::current();
+    println!("thread {:?}", thread.id());
+
     let new = CustomMenuItem::new("new", "New");
     let open = CustomMenuItem::new("open", "Open");
     let save = CustomMenuItem::new("save", "Save");
